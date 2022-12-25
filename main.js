5bff73527d912a9789906ba63428423c9d39fae9
jossefo <yossidv@hotmail.com>
2022-12-25 15:00:26 +0200

    removed files + updating main and blockchain

--- main.js
+++ main.js
@@ -1,8 +1,29 @@
-const {BlockChain,Block}=require('./blockchain.js')
+const {BlockChain,Block, Transaction}=require('./blockchain.js')
+const EC = require('elliptic').ec 
+const ec = new EC('secp256k1')
+
+const my_key = ec.keyFromPrivate('695c7100b179d88ebe913ee64f09e0cf7247fbc96d81f8c7d771db2edabedd4d')
+
+const my_wallet_address = my_key.getPublic('hex')
 
 let JossefCoin = new BlockChain()
 
-JossefCoin.addBlock(new Block(1,"13/11/2022",{amount:4}))
-JossefCoin.addBlock(new Block(1,"13/11/2022",{amount:4}))
-console.log(JSON.stringify(JossefCoin,null,4))
+const tx1 = new Transaction(my_wallet_address,'address1',15)
+tx1.sing_transaction(my_key)
+JossefCoin.add_transaction(tx1)
 
+JossefCoin.miningPendingTransaction(my_wallet_address)
+
+const tx2 = new Transaction(my_wallet_address,'address2',15)
+tx2.sing_transaction(my_key)
+JossefCoin.add_transaction(tx2)
+
+// const tx3 = new Transaction(my_wallet_address,'address2',40)
+// tx2.sing_transaction(my_key)
+// JossefCoin.add_transaction(tx2)
+
+console.log()
+console.log(`Balace of JossefCoin is ${JossefCoin.get_Balance_of_address('address2')}`)
+console.log('--------------')
+console.log(JossefCoin.chain)
+console.log()
